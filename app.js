8dee099bf9fb1a26e0f2665c16400a3006e2e197
Aaron <aaronsr@gmail.com>
2022-09-14 14:34:56 -0700

    Inital commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (reg, res) => res.send('Hello World'))
+
+app.listen(port)
+console.log(`App is running on http://localhost:${port}`)
+
