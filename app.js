ff0f615402aba39376bb7c0fccfc934891265fd3
Aaron <aaronsr@gmail.com>
2022-09-15 14:25:40 -0700

    Update app.jd message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (reg, res) => res.send('Hello World'))
+app.get('/', (reg, res) => res.send('Hello World, I am alive!'))
 
 app.listen(port)
 console.log(`App is running on http://localhost:${port}`)
